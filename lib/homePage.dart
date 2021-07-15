d525af297e138f88003ab4a75f33ae786bd5fcf3
fathiayari <fathi3ayari333@gmailcom>
2021-07-15 10:37:14 +0200

    elevatedButton radius

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -15,60 +15,91 @@ class _HomePageState extends State<HomePage> {
     return
 Scaffold(
   backgroundColor: Color(0xFF21222E),
-  body: Column(
+  body: SingleChildScrollView(
+    child: Column(
 
-    children: [
-      SizedBox(
-        height: 100 ,
-      ),
-      Container(
-        child :Image.network('https://m.media-amazon.com/images/G/01/imdb/images/social/imdb_logo._CB410901634_.png'),
-        width: 200,
-        height: 180,
-       /* child: Image.network('https://m.media-amazon.com/images/G/01/imdb/images/social/imdb_logo._CB410901634_.png',)*/
+      children: [
+        SizedBox(
+          height: 100 ,
+        ),
+        Container(
+          child :Image.network('https://m.media-amazon.com/images/G/01/imdb/images/social/imdb_logo._CB410901634_.png'),
+          width: 200,
+          height: 180,
+         /* child: Image.network('https://m.media-amazon.com/images/G/01/imdb/images/social/imdb_logo._CB410901634_.png',)*/
 
-      ),
-      SizedBox(
-        height: 50 ,
-      ),
-      Text('IMDb  is an online database of information related to films, television programs, home videos.',
-        textAlign: TextAlign.center,
-        style: TextStyle(
-        color: Colors.white,
-        fontSize: 18,
+        ),
+        SizedBox(
+          height: 50 ,
+        ),
+        Text('IMDb  is an online database of information related to films, television programs, home videos.',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
 
-      ),),
-      SizedBox(
-        height: 50 ,
-      ),
-      Row(
-        children: [
-          Expanded(child: TextFormField(
+        ),),
+        SizedBox(
+          height: 50 ,
+        ),
+        Row(
+          children: [
+            Expanded(child: TextFormField(
 
-          decoration:InputDecoration(
+            decoration:InputDecoration(
 
-            hintText: 'Movie name', //Plceholder
-            hintStyle: TextStyle(
-              color: Colors.grey,// the color of hintText
-              fontSize: 18
+              hintText: 'Movie name', //Plceholder
+              hintStyle: TextStyle(
+                color: Colors.grey,// the color of hintText
+                fontSize: 18
+
+              ),
+              fillColor: Color(0xFF2a2b37),// the color of the inside box field
+              filled: true,
+
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20) //borderradius
+              )
+
+
+
 
             ),
-            fillColor: Color(0xFF2a2b37),// the color of the inside box field
-            filled: true,
 
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20)
-            )
+              )),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 50,
+              child: ElevatedButton.icon(onPressed: (){},
+
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.amber,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
 
 
+                    /*style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: BorderSide(color: Colors.red)
+                            )
+                        )
+                    )*/
 
-
-          ),
-
-            ))
-        ],
-      )
-    ],
+                ),
+              label: Text('Search'),
+              icon:Icon(Icons.search),),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+          ],
+        )
+      ],
+    ),
   ),
 
 
