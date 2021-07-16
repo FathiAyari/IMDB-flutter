1425a62bbbbb5b7635cac82a8eff692730d49bf0
fathiayari <fathi3ayari333@gmailcom>
2021-07-16 11:25:39 +0200

    set up the no connexion page

--- lib/noConnexion.dart
+++ lib/noConnexion.dart
@@ -11,23 +11,23 @@ class _noConnexionState extends State<noConnexion> {
   Widget build(BuildContext context) {
     return Scaffold(
       body:Column(
-        
+
         children: [
           SizedBox(
             height: 140,
           ),
           Container(
 
-            height: 300,
-            //width: double.infinity  ,// take the whole space in width
-            child: FittedBox( fit: BoxFit.cover ,
+              height: 300,
+              //width: double.infinity  ,// take the whole space in width
+              child: FittedBox( fit: BoxFit.cover ,
 
 
-              // use the fitted box for the adjustement
-                child: Image(image: AssetImage('assets/images/network.png'),))
+                  // use the fitted box for the adjustement
+                  child: Image(image: AssetImage('assets/images/network.png'),))
 
 
-            ),
+          ),
 
 
 
